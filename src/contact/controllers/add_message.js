7093755ebf4f8e392fed90eaa2c5f549cb3c0906
Kamal Nanda <kamalnanda@Kamals-MacBook-Air.local>
2025-03-05 22:47:38 +0530

    contact me apis

--- src/contact/controllers/add_message.js
+++ src/contact/controllers/add_message.js
@@ -5,10 +5,10 @@ export const add_message = async (req, res) => {
     const reqId = res.locals.uuid
     try{
         await Contacts.create(req.body);
-        return res.status(200)
+        return res.status(200).json({status:true})
     } catch (error){
         Logger(reqId).error(`Error in add_message - ${error.message}`)
         console.log(error)
-        return res.status(500)
+        return res.status(500).json({status:true})
     }
 }
